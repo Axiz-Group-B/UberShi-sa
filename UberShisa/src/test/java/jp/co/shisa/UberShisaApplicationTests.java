b40f136eeb21a7f386bdb33fc84533644225ba40
BoRua <redphale@gmail.com>
2021-06-16 19:52:47 +0900

    push for shisa

---
+++ UberShisa/src/test/java/jp/co/shisa/UberShisaApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.shisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberShisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
