97b7c72ae6fe8ddf829bb62a124d83219d5f3aff
User <User@WINDOWS-SIN58PA>
2021-06-21 19:22:12 +0900

    logout機能追加

--- UberShisa/src/main/java/jp/co/shisa/controller/AuthController.java
+++ UberShisa/src/main/java/jp/co/shisa/controller/AuthController.java
@@ -27,52 +27,70 @@ public class AuthController {
 	@Autowired
 	HttpSession session;
 
-	@RequestMapping({"/", "/index"})
+	@RequestMapping({ "/", "/index" })
 	public String index(Model model) {
 		return "index.html";
 	}
 
 	@RequestMapping("/login")
-	public String login(@ModelAttribute("login")LoginForm form,BindingResult bindingResult,Model model,RedirectAttributes attr) {
-			if(bindingResult.hasErrors()) {
-				return "index";
-			}
+	public String login(@ModelAttribute("login") LoginForm form, BindingResult bindingResult, Model model,
+			RedirectAttributes attr) {
+		if (bindingResult.hasErrors()) {
+			return "index";
+		}
 
-			UserInfo userInfo = authService.loginCheck(form);
-			if(userInfo == null) {
-				String errorMsg = "IDまたはPASSが間違っています";
-				attr.addFlashAttribute("errorMsg", errorMsg);
-				return "redirect:index";
-			}
+		UserInfo userInfo = authService.loginCheck(form);
+		if (userInfo == null) {
+			String errorMsg = "IDまたはPASSが間違っています";
+			attr.addFlashAttribute("errorMsg", errorMsg);
+			return "redirect:index";
+		}
 
-			Integer roleId = userInfo.getRoleId();
+		Integer roleId = userInfo.getRoleId();
 
-			switch(roleId) {
-			case 1:
-				Room room = authService.loginByRoom(userInfo);
-				session.setAttribute("loginUser", room);
-				return "order";
+		switch (roleId) {
+		case 1:
+			Room room = authService.loginByRoom(userInfo);
+			session.setAttribute("loginUser", room);
+			return "order";
 
-			case 2:
-				DeliveryMan deliveryMan = authService.loginByDeliveryMan(userInfo);
-				session.setAttribute("loginUser", deliveryMan);
-				return "delivery";
-			case 3:
-				Shop shop = authService.loginByShop(userInfo);
-				session.setAttribute("loginUser",shop);
-				return "store";
+		case 2:
+			DeliveryMan deliveryMan = authService.loginByDeliveryMan(userInfo);
+			session.setAttribute("loginUser", deliveryMan);
+			return "delivery";
+		case 3:
+			Shop shop = authService.loginByShop(userInfo);
+			session.setAttribute("loginUser", shop);
+			return "store";
 
-			case 4:
-				session.setAttribute("loginUser",userInfo);
-				return "hotel";
-			default:
-				String errorMsg = "IDまたはPASSが間違っています";
-				attr.addFlashAttribute("errorMsg", errorMsg);
-				return "redirect:";
-			}
-
+		case 4:
+			session.setAttribute("loginUser", userInfo);
+			return "hotel";
+		default:
+			String errorMsg = "IDまたはPASSが間違っています";
+			attr.addFlashAttribute("errorMsg", errorMsg);
+			return "redirect:";
+		}
 
 	}
 
+
+
+
+
+
+
+
+
+
+	/*
+	* ログアウト
+	*/
+	@RequestMapping("/logout")
+	public String logout(Model model) {
+		session.invalidate();
+		return "index";
+		//indexに遷移
+	}
 
 }
