141ba2f9ce51f4291a9bd9fe1609cce511521283
kawa <21kawamoto_yuka@axiz.co.jp>
2021-06-21 20:05:11 +0900

    コントローラー

--- UberShisa/src/main/java/jp/co/shisa/controller/RoomController.java
+++ UberShisa/src/main/java/jp/co/shisa/controller/RoomController.java
@@ -9,7 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
-import jp.co.shisa.entity.RoomOrderForm;
 import jp.co.shisa.entity.Shop;
 import jp.co.shisa.service.ShopService;
 
