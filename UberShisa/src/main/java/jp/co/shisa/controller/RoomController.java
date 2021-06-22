32fdbdf28411acf814dfdf150934b21091720fc3
BoRua <redphale@gmail.com>
2021-06-22 09:19:23 +0900

    m

--- UberShisa/src/main/java/jp/co/shisa/controller/RoomController.java
+++ UberShisa/src/main/java/jp/co/shisa/controller/RoomController.java
@@ -1,5 +1,34 @@
 package jp.co.shisa.controller;
 
-public class RoomController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import jp.co.shisa.entity.Shop;
+import jp.co.shisa.service.ShopService;
+
+@Controller
+@EnableAutoConfiguration
+public class RoomController {
+	@Autowired
+	private ShopService shopS;
+
+	@GetMapping("/room/order")
+	public String order(Model model) {
+
+		List<Shop> list = shopS.findAll();
+		model.addAttribute("shopList", list);
+
+		return "order";
+	}
+
+//	@GetMapping("/room/select")
+//	public String roomSelect(@ModelAttribute("roomSelect") RoomOrderForm form, Model model) {
+//		//formの、店舗idで検索
+//		return "order";
+//	}
 }
