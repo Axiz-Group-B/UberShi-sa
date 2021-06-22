23e413397d057a94740824383308e3fce6bf366b
User <User@WINDOWS-SIN58PA>
2021-06-22 10:37:53 +0900

    コミット

--- UberShisa/src/main/java/jp/co/shisa/service/HotelService.java
+++ UberShisa/src/main/java/jp/co/shisa/service/HotelService.java
@@ -1,9 +1,6 @@
 package jp.co.shisa.service;
 
-<<<<<<< HEAD
-public interface HotelService {
 
-=======
 import java.util.List;
 
 import jp.co.shisa.entity.OrderInfo;
@@ -13,5 +10,5 @@ public interface HotelService {
 	public List<Shop> shopFindAll();
 
 	public List<OrderInfo> orderInfoFindAll();
->>>>>>> refs/remotes/origin/main
+
 }
