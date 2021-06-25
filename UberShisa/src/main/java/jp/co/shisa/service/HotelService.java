c3d5f87658b590142d0f90ad495f043fa659ec60
S.D. <21daito_sea@axiz.co.jp>
2021-06-25 10:55:00 +0900

    注文履歴に年を含んだ検索を追加

--- UberShisa/src/main/java/jp/co/shisa/service/HotelService.java
+++ UberShisa/src/main/java/jp/co/shisa/service/HotelService.java
@@ -9,12 +9,12 @@ import jp.co.shisa.entity.Shop;
 public interface HotelService {
 
 	public List<Shop> shopFindAll();
-	public List<OrderInfo> orderInfoFind(Integer shopId,String dateTime);
+	public List<OrderInfo> orderInfoFind(Integer shopId,String year,String month);
 	public List<DeliveryMan> DeliveryManFindAll();
 	public List<OrderInfo> OrderInfoFindId(Integer orderId);
 	public void UserInfoDelete(Integer deliveryManId);
 	public void DeliveryManDelete(Integer deliveryManId);
-	public Integer totalPrice(Integer shopId,String dateTime);
+	public Integer totalPrice(Integer shopId,String year,String month);
 	public Integer priceSum(Integer orderId);
 	public void hotelUserInfoDelete(Integer shopId);
 	public void HotelShopDelete(Integer shopId);
