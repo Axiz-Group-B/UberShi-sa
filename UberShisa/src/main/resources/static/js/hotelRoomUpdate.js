2951be4f886c367305f30a88d12b1969bdb5fafb
kawa <21kawamoto_yuka@axiz.co.jp>
2021-06-28 16:15:38 +0900

    修正

--- UberShisa/src/main/resources/static/js/hotelRoomUpdate.js
+++ UberShisa/src/main/resources/static/js/hotelRoomUpdate.js
@@ -2,7 +2,7 @@ function checkUpdateRoom(){
 	if(window.confirm('更新してよろしいですか？')){
 		location.href="/hotel/updateIdAndPass"
 	}else{
-		window.alert('キャンセルされました');
+		//window.alert('キャンセルされました');
 		return false;
 	}
 }
